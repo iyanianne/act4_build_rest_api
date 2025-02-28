9d9b62e7f77f18e85e45a672f1680c9c8a1e1304
unknown <YY408@ccs.local>
2025-02-28 21:16:35 +0800

    Initial commit with REST_API_FILE file

---
+++ src/users/users.routes.ts
@@ -0,0 +1,57 @@
+import express, {Request, Response} from "express"
+import { UnitUser, User } from "./users.interface"
+import {StatusCodes} from "http-status-codes"
+import * as database from "./users.database"
+
+export const userRouter = express.Router()
+
+userRouter.get("/users", async (req : Request, res : Response) => {
+    try {
+        const allUsers : UnitUser[] = await database.findAll()
+
+        if (!allUsers) {
+            return res.status(StatusCodes.NOT_FOUND).json({msg : `No users at this time...`})
+        }
+
+        return res.status(StatusCodes.OK).json({total_user : allUsers.length, allUsers})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+userRouter.get("./user/:id", async (req : Request, res : Response) => {
+    try {
+        const user : UnitUser = await database.findOne(req.params.id)
+
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({error : `User not found!`})
+        }
+
+        return res.status(StatusCodes.OK).json({user})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+userRouter.post("/register", async (req : Request, res : Response) => {
+    try {
+        const { username , email, password } =  req.body
+
+        if (!username || !email || !password) {
+            return res.status(StatusCodes.BAD_REQUEST).json({error : `Please provide all the required parameters...`})
+        }
+
+        const user = await database.findByEmail(email)
+
+        if (user) {
+            return res.status(StatusCodes.BAD_REQUEST).json({error : `This email has already been registered...`})
+        }
+
+        const newUser = await database.create(req.body)
+
+        return res.status(StatusCodes.CREATED).json({newUser})
+    
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
