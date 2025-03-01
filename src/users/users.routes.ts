5c03aa146a0f68f71834bf4c4a0713917fd7ab25
MGB7 IO Heart Rizarri <mariannemaecalderon04@gmail.com>
2025-03-01 20:19:12 +0800

    Final commit with REST-API-FILE

--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from "express"
-import { UnitUser, User } from "./users.interface"
+import { UnitUser, User } from "./user.interface"
 import {StatusCodes} from "http-status-codes"
-import * as database from "./users.database"
+import * as database from "./user.database"
 
 export const userRouter = express.Router()
 
@@ -19,7 +19,7 @@ userRouter.get("/users", async (req : Request, res : Response) => {
     }
 })
 
-userRouter.get("./user/:id", async (req : Request, res : Response) => {
+userRouter.get("/user/:id", async (req : Request, res : Response) => {
     try {
         const user : UnitUser = await database.findOne(req.params.id)
 
@@ -59,6 +59,7 @@ userRouter.post("/register", async (req : Request, res : Response) => {
 userRouter.post("/login", async (req : Request, res : Response) => {
     try {
         const {email, password} = req.body
+
         if (!email || !password) {
             return res.status(StatusCodes.BAD_REQUEST).json({error : "Please provide all the required parameters.."})
         }
@@ -68,5 +69,60 @@ userRouter.post("/login", async (req : Request, res : Response) => {
         if (!user) {
             return res.status(StatusCodes.NOT_FOUND).json({error : "No user exist with email provided..."})
         }
+
+        const comparePassword = await database.comparePassword(email, password)
+
+        if (!comparePassword) {
+            return res.status(StatusCodes.BAD_REQUEST).json({error : `Incorrect Password!`})
+        }
+
+        return res.status(StatusCodes.OK).json({user})
+    
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
+    }
+})
+
+
+userRouter.put('/user/:id', async (req : Request, res : Response) => {
+
+    try {
+
+        const {username, email, password} = req.body
+
+        const getUser = await database.findOne(req.params.id)
+
+        if (!username || !email || !password) {
+            return res.status(401).json({error : `Please proovide all the required parameters...`})
+        }
+
+        if (!getUser) {
+            return res.status(404).json({error : `No user wiith id ${req.params.id}`})
+        }
+
+        const updateUser = await database.update((req.params.id), req.body)
+
+        return res.status(201).json({updateUser})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error})
+    }
+})
+
+userRouter.delete("/user/:id", async (req : Request, res : Response) => {
+    try {
+        const id = (req.params.id)
+
+        const user = await database.findOne(id)
+
+        if (!user) {
+            return res.status(StatusCodes.NOT_FOUND).json({error : `User does not exist`})
+        }
+
+        await database.remove(id)
+
+        return res.status(StatusCodes.OK).json({msg : "User deleted"})
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({error})
     }
 })
